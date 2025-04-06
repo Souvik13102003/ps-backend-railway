99b0b60f90da47346d149d5d4b7e5c8b3d6e0cd5
Souvik13102003 <93079530+Souvik13102003@users.noreply.github.com>
2025-04-06 19:29:57 +0530

    updatesss

--- controllers/billing.controller.js
+++ controllers/billing.controller.js
@@ -8,6 +8,7 @@ const path = require('path');
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
+
 // Supabase Setup
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
